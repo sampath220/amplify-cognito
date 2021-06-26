b9abfcdd55b1829850ad5d118f0db2bd4bfaf4e3
Sampath Kumar Reddy <ssampathkumarredd@deloitte.com>
2021-06-26 08:21:48 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
 
 function App() {
   return (
     <div className="App">
       <div>hello</div>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
